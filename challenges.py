53bc87367e15a6a1fe677a2c117dee8be7413ae0
Pablo del Hoyo <pablodelhoyo1314@gmail.com>
2022-05-13 21:48:53 +0200

    Finish yinkana

--- challenges.py
+++ challenges.py
@@ -1,4 +1,6 @@
 import socket
+import urllib.parse
+from concurrent.futures import ThreadPoolExecutor
 import hashlib
 from typing import Optional
 
@@ -12,8 +14,8 @@ def first(identifier: str) -> str:
         print(sock.recv(utils.MAX_BYTES).decode())
         sock.sendall(identifier.encode())
 
-        data = utils.read_until(sock)
-        return utils.get_identifier_from_data(data)
+        data = utils.read_until_end(sock).decode()
+        return utils.get_identifier_or_raise_exception(data)
 
 
 def second(identifier: str) -> str:
@@ -26,7 +28,7 @@ def second(identifier: str) -> str:
         sock.sendto(identifier.upper().encode(), addr)
         data = sock.recvfrom(utils.MAX_BYTES)[0].decode()
         print(data)
-        return utils.get_identifier_from_data(data)
+        return utils.get_identifier_or_raise_exception(data)
 
 def third(identifier: str) -> str:
     end_delimiter = "that's the end"
@@ -37,10 +39,10 @@ def third(identifier: str) -> str:
         words = len(large_text[:large_text.index(end_delimiter)].split())
 
         sock.sendall(f"{identifier} {words}".encode())
-        remaining_data = utils.read_until(sock)
+        remaining_data = utils.read_until_end(sock).decode()
 
         print(remaining_data)
-        return utils.get_identifier_from_data(remaining_data)
+        return utils.get_identifier_or_raise_exception(remaining_data)
 
 def fourth(identifier: str) -> str:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -59,17 +61,16 @@ def fourth(identifier: str) -> str:
 
         sock.sendall(f"{identifier} {decoded_msg} --".encode())
 
-        remaining_data = utils.read_until(sock)
+        remaining_data = utils.read_until_end(sock).decode()
 
         print(remaining_data)
-        return utils.get_identifier_from_data(remaining_data)
+        return utils.get_identifier_or_raise_exception(remaining_data)
 
 def fifth(identifier: str) -> str:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((utils.HOST, 9003))
         sock.sendall(identifier.encode())
 
-        # TODO: make this more robust
         first_part = sock.recv(utils.MAX_BYTES)
         colon_index = first_part.index(b":")
         remaining_data = int(first_part[:colon_index].decode()) - (len(first_part) - colon_index - 1)
@@ -81,10 +82,10 @@ def fifth(identifier: str) -> str:
             remaining_data -= len(data)
 
         sock.sendall(sha1.digest())
-        last_data = utils.read_until(sock)
+        last_data = utils.read_until_end(sock).decode()
 
         print(last_data)
-        return utils.get_identifier_from_data(last_data)
+        return utils.get_identifier_or_raise_exception(last_data)
 
 def sixth(identifier: str) -> str:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
@@ -94,6 +95,53 @@ def sixth(identifier: str) -> str:
         msg = yap.to_yap_packet(raw_recieve).payload.decode()
 
         print(msg)
-        return utils.get_identifier_from_data(msg)
+        return utils.get_identifier_or_raise_exception(msg)
 
-all_challenges = [first, second, third, fourth, fifth, sixth]
+def handle_connection(connection: socket.socket, get_request: bytes):
+    end_resource_idx = get_request.index(b"HTTP") - 1
+    resource = b"/rfc" + get_request[4:end_resource_idx]
+    get_request = b"GET " + resource + get_request[end_resource_idx:]
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_get:
+        sock_get.connect((utils.HOST, 81))
+        sock_get.sendall(get_request)
+        data = utils.read_until_end(sock_get)
+
+    connection.sendall(data)
+
+def seventh(identifier: str) -> str:
+    # Socket to send the port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_server:
+        port = utils.try_bind_socket(sock_server)
+        sock_server.listen()
+
+        # Send the port to rick:8003
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_send_port:
+            sock_send_port.connect((utils.HOST, 8003))
+            sock_send_port.sendall(f"{identifier} {port}".encode())
+
+
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            while True:
+                conn, _ = sock_server.accept();
+                get_request = conn.recv(utils.MAX_BYTES)
+
+                unquoted_request = urllib.parse.unquote_plus(get_request.decode("ascii"))
+                next_id = utils.get_identifier_from_data(unquoted_request)
+
+                if next_id is not None:
+                    print(unquoted_request)
+                    break
+
+                executor.submit(handle_connection, conn, get_request)
+
+    return next_id
+
+def end(identifier: str) -> str:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((utils.HOST, 33333))
+        sock.sendall(identifier.encode())
+        print(sock.recv(utils.MAX_BYTES).decode())
+    return ""
+
+all_challenges = [first, second, third, fourth, fifth, sixth, seventh, end]
--- utils.py
+++ utils.py
@@ -8,6 +8,12 @@ MAX_BYTES = 4096
 ID_PREFIX = "identifier"
 MIN_DYNAMIC_PORT, MAX_DYNAMIC_PORT = 49152, 65535
 
+def get_identifier_or_raise_exception(data: str) -> str:
+    identifier = get_identifier_from_data(data)
+    if identifier is not None:
+        return identifier
+    raise ValueError("indentifier was not found in the provided data")
+
 def decrypt_cesar_word(word: str, key: int) -> str:
     decoded_word = ""
     for char in word:
@@ -24,12 +30,26 @@ def first_digit_index(data: str) -> Optional[int]:
 
     return None
 
-def get_identifier_from_data(data: str) -> str:
+def get_identifier_from_data(data: str) -> Optional[str]:
     """ Returns the identifier. If ID_PREFIX is not in data then an exception is raised """
+    idx = data.find(ID_PREFIX)
+    if idx != -1:
+        return data[idx + len(ID_PREFIX) + 1:data.index("\n")]
+    return None
 
-    return data[data.index(ID_PREFIX) + len(ID_PREFIX) + 1:data.index("\n")]
+def read_until_end(socket: socket.socket) -> bytes:
+    all_data = b""
+    while True:
+        data = socket.recv(MAX_BYTES)
+        if not data:
+            break
 
-def read_until(socket: socket.socket, delimiter = None) -> str:
+        all_data += data
+
+    return all_data
+
+
+def read_until(socket: socket.socket, delimiter, encoding: str = "utf-8") -> str:
     """ Reads until delimiter is found. If delimiter is None (the default), it
     will read until the remote host closes the connection """
 
@@ -38,7 +58,7 @@ def read_until(socket: socket.socket, delimiter = None) -> str:
         data = socket.recv(MAX_BYTES)
         if not data:
             break
-        all_data += data.decode()
+        all_data += data.decode(encoding)
 
         if delimiter and delimiter in all_data:
             return all_data
